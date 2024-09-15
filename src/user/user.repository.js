788bef62cfa659fcadad74e207b726a94d0d0921
rach-syahputra <rach.syahputra@gmail.com>
2024-09-16 07:50:52 +0800

    implement secure password hashing when creating user

--- src/user/user.repository.js
+++ src/user/user.repository.js
@@ -1,9 +1,15 @@
 const prisma = require('../db')
+const bcrypt = require('bcrypt')
 
 const findUserByUsername = async (username) => {
   const user = await prisma.user.findUnique({
     where: {
       username
+    },
+    select: {
+      username: true,
+      email: true,
+      roleId: true
     }
   })
 
@@ -11,11 +17,14 @@ const findUserByUsername = async (username) => {
 }
 
 const createUser = async (userData) => {
+  const salt = await bcrypt.genSalt()
+  const hashedPassword = await bcrypt.hash(userData.password, salt)
+
   const user = await prisma.user.create({
     data: {
       username: userData.username,
       email: userData.email,
-      password: userData.password,
+      password: hashedPassword,
       roleId: userData.roleId
     }
   })
