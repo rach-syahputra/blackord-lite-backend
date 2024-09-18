6ff4c430adb586665a7ceff0b6ec0426f8e50df4
rach-syahputra <rach.syahputra@gmail.com>
2024-09-18 13:15:57 +0800

    add findUserByUsername function to auth repository

--- src/auth/auth.repository.js
+++ src/auth/auth.repository.js
@@ -1,10 +1,27 @@
 const prisma = require('../db')
 
 const authRepository = {
+  async findUserByUsername(username) {
+    const user = await prisma.user.findUnique({
+      where: {
+        username
+      }
+    })
+
+    return user
+  },
+
   async findUserByRefreshToken(refreshToken) {
     const user = await prisma.user.findMany({
       where: {
         refreshToken
+      },
+      select: {
+        username: true,
+        email: true,
+        roleId: true,
+        createdAt: true,
+        refreshToken: true
       }
     })
 
