cc689b450e54f75dfc998ca6d2d3271989c93042
rach-syahputra <rach.syahputra@gmail.com>
2024-09-18 13:15:18 +0800

    implement verifyToken middleware for logout route

--- src/auth/auth.route.js
+++ src/auth/auth.route.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const authController = require('./auth.controller')
+const { verifyToken } = require('../middleware/verification')
 
 const router = express.Router()
 
 router.post('/login', authController.login)
 router.get('/refresh-token', authController.refreshToken)
-router.delete('/logout', authController.logout)
+router.delete('/logout', verifyToken, authController.logout)
 
 module.exports = router
