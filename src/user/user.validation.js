1e5ec78fd839e45d6bc5a58a820bc31daf818577
rach-syahputra <rach.syahputra@gmail.com>
2024-09-18 21:25:33 +0800

    add error messages in add user form validation

--- src/user/user.validation.js
+++ src/user/user.validation.js
@@ -1,10 +1,16 @@
 const { z } = require('zod')
 
 const AddUserSchema = z.object({
-  username: z.string().max(20, 'Username should not exceed 20 characters'),
-  email: z.string().email('Email format is invalid'),
+  username: z
+    .string()
+    .min(1, 'Username is required')
+    .max(20, 'Username should not exceed 20 characters'),
+  email: z
+    .string()
+    .min(1, 'Email is required')
+    .email('Email format is invalid'),
   password: z.string().min(8, 'Password must contain at least 8 characters'),
-  roleId: z.number(),
+  roleId: z.number().min(1, 'Role ID is required'),
   refreshToken: z.string().optional()
 })
 
