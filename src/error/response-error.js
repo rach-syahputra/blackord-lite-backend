2f063a2410495f8aa009ada27a3e381fb4f3254b
rach-syahputra <rach.syahputra@gmail.com>
2024-09-18 13:58:17 +0800

    implement ResponseError parameter types check

--- src/error/response-error.js
+++ src/error/response-error.js
@@ -1,5 +1,12 @@
 class ResponseError extends Error {
   constructor(status, message) {
+    if (typeof status !== 'number') {
+      throw new TypeError('Status must be a number')
+    }
+    if (typeof message !== 'string') {
+      throw new TypeError('Message must be a string')
+    }
+
     super(message)
     this.status = status
   }
