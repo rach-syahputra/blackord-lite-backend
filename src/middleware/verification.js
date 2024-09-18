4adc3c582dce2c51d2226299a97aea15bd436217
rach-syahputra <rach.syahputra@gmail.com>
2024-09-18 13:14:51 +0800

    implement error handling using ResponseError in verification middleware

--- src/middleware/verification.js
+++ src/middleware/verification.js
@@ -1,20 +1,17 @@
 const jwt = require('jsonwebtoken')
+const { ResponseError } = require('../error/response-error')
 
 const verification = {
   verifyToken(req, res, next) {
     const authHeader = req.headers['authorization']
     const token = authHeader && authHeader.split(' ')[1]
     if (token == null) {
-      return res.status(401).json({
-        message: 'Unauthorized'
-      })
+      throw new ResponseError(401, 'You are unauthorized')
     }
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
       if (err) {
-        return res.status(403).json({
-          message: 'Token has been expired'
-        })
+        throw new ResponseError(403, 'Token has been expired')
       }
 
       req.username = decoded.username
