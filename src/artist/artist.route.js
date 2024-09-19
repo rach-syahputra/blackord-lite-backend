c22f05e59cd8b4664b4b6a003cc62cc8713f70f7
rach-syahputra <rach.syahputra@gmail.com>
2024-09-19 16:49:17 +0800

    add getFollowersFromArtist, followArtist and unfollowArtist routes

--- src/artist/artist.route.js
+++ src/artist/artist.route.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const artistController = require('./artist.controller')
 const { verifyToken } = require('../middleware/verification')
+const listenerArtistController = require('../relationship/listener-artist/listener-artist.controller')
 
 const router = express.Router()
 
@@ -8,5 +9,20 @@ router.get('/', verifyToken, artistController.getAllArtists)
 router.get('/:username', verifyToken, artistController.getArtist)
 router.post('/', artistController.addArtist)
 router.put('/:username', verifyToken, artistController.updateArtist)
+
+router.get(
+  '/:username/followers',
+  listenerArtistController.getAllFollowersFromArtist
+)
+router.post(
+  '/:username/follow',
+  verifyToken,
+  listenerArtistController.addFollowedArtist
+)
+router.post(
+  '/:username/unfollow',
+  verifyToken,
+  listenerArtistController.deleteFollowedArtist
+)
 
 module.exports = router
