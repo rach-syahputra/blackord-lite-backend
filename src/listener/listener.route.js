eb8cac3069af4ba481fa3c31c18e16e40b42f27d
rach-syahputra <rach.syahputra@gmail.com>
2024-09-19 16:48:27 +0800

    add getFavoriteArtists route

--- src/listener/listener.route.js
+++ src/listener/listener.route.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const { verifyToken } = require('../middleware/verification')
 const listenerController = require('./listener.controller')
+const listenerArtistController = require('../relationship/listener-artist/listener-artist.controller')
 
 const router = express.Router()
 
 router.get('/:username', verifyToken, listenerController.getListener)
 router.post('/', listenerController.addListener)
 router.put('/:username', verifyToken, listenerController.updateListener)
+
+router.get(
+  '/:username/favorite-artists',
+  listenerArtistController.getAllFavoriteArtistsFromListener
+)
 
 module.exports = router
