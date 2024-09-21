e9efe1d8181fc340d109b3a2ac2dd519f0d292dd
rach-syahputra <rach.syahputra@gmail.com>
2024-09-21 20:08:39 +0800

    add uploadArtistImage multer

--- src/utils/multer/index.js
+++ src/utils/multer/index.js
@@ -1,18 +1,30 @@
 const multer = require('multer')
-const { imageFilter, MAX_FILE_SIZE } = require('./filter')
+const {
+  imageFilter,
+  MAX_LISTENER_IMAGE_SIZE,
+  MAX_ARTIST_IMAGE_SIZE
+} = require('./filter')
 
-const listenerImageStorage = multer.diskStorage({
+const storage = multer.diskStorage({
   filename: function (req, file, cb) {
     cb(null, file.originalname)
   }
 })
 
 const uploadListenerImage = multer({
-  storage: listenerImageStorage,
+  storage,
   fileFilter: imageFilter,
   limits: {
-    fileSize: MAX_FILE_SIZE
+    fileSize: MAX_LISTENER_IMAGE_SIZE
   }
 })
 
-module.exports = { uploadListenerImage }
+const uploadArtistImage = multer({
+  storage,
+  fileFilter: imageFilter,
+  limits: {
+    fileSize: MAX_ARTIST_IMAGE_SIZE
+  }
+})
+
+module.exports = { uploadListenerImage, uploadArtistImage }
