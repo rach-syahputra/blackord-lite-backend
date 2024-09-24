04efcf5b682e3a756567fe85e3fa9e6236062c13
rach-syahputra <rach.syahputra@gmail.com>
2024-09-24 18:14:06 +0800

    implement cors

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const cors = require('cors')
 const cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const { ResponseError } = require('./utils/error/response-error')
@@ -12,6 +13,8 @@ const albumRoutes = require('./resources/album/album.route')
 const songRoutes = require('./resources/song/song.route')
 
 const app = express()
+
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 app.use(bodyParser.json())
